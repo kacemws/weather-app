db875b017c48f8a69e116cd87ed6fe01eabc0611
kacemws <berras.belkacem@gmail.com>
2021-08-14 23:27:57 +0200

    fix: animating
    Adding animation using the framer motion package

--- src/components/Left.js
+++ src/components/Left.js
@@ -1,7 +1,23 @@
+import { motion } from "framer-motion";
 function Left() {
+  const word = {
+    initial: {
+      y: 400,
+      opacity: 0,
+    },
+    animate: {
+      y: [200, -50, 0],
+      opacity: 1,
+      transition: { duration: 1 },
+    },
+  };
   return (
     <div className="left">
-      <h1>Slick weather</h1>
+      <motion.h1 variants={word} initial="initial" animate="animate">
+        <span>Slick</span>
+        <br />
+        <span>weather</span>
+      </motion.h1>
       <h3>Checking weather was never this fancy !</h3>
     </div>
   );
